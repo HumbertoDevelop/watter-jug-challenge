e30e5e3a588b228eaeb11fe3343590ada805e50a
Humberto García <humbertodev14@gmail.com>
2024-11-10 18:26:30 -0400

    config deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://humbertodevelop.github.io/watter-jug-challenge'
 })
